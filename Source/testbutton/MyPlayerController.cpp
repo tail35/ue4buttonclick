7bdf3425790e150564871f38c259c3d841d72784
tail35 <ragingfire35@gmail.com>
2019-11-26 19:26:27 +0800

    add player controler file

---
+++ Source/testbutton/MyPlayerController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPlayerController.h"
+
+
+
+void AMyPlayerController::BeginPlay() {
+	int i = 0;
+	i++;
+	UE_LOG(LogTemp, Warning, TEXT("Problem111!"));
+}
